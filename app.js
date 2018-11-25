d70e5acdb98f7b865d19e556797ae96b5444e367
Oleksiy <doubledare704@gmail.com>
2018-11-25 21:31:30 +0200

    update app.js

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-new Vue({
-  el: '#app',
-  data: function() {
-    return {
-      movie: "",
-      lang: "",
-      default_langs: ["ru", "ua", "en"]
-    }
-  },
-  methods: {
-    searchMovie() {
+$(function() {
+  let compatibleBrowser = typeof Object['__defineSetter__'] === 'function';
+  if (compatibleBrowser) {
+    new Vue({
+      el: '#app',
+      data: function() {
+        return {
+          movie: "",
+          lang: "",
+        }
+      },
+      methods: {
+        searchMovie() {
+          let yoho = document.getElementById("yohoho");
+          yoho.dataset.title = this.movie;
+          yoho.dataset.language = this.lang;
+          yo();
+        }
+      }
+    })
+  } else {
+    let search = $("#search").click(function() {
+      let name = $("#movieName").val();
+      let lang = $("#movieLang").val();
       let yoho = document.getElementById("yohoho");
-      yoho.dataset.title = this.movie;
-      yoho.dataset.language = this.lang;
+      yoho.dataset.title = name;
+      yoho.dataset.language = lang;
       yo();
-    }
+
+    })
   }
-})
+});
